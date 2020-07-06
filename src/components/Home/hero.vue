cceb4de42ed9551a3b96fbec3156d0c5a5a1c9cb
Prince <chimeremezechukwudire@gmail.com>
2020-07-06 12:53:08 +0100

    hero added

---
+++ src/components/Home/hero.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <!-- Hero here -->
+    <section class="hero" id="home">
+      <div class="container text-light">
+        <div class="row">
+          <div class="col-md-6 col-lg-6 col-sm-12">
+          <h3>Carol danvers</h3>
+            <h1 class="hero-text">
+              <strong>
+              captain marvel
+              </strong>
+            </h1>
+          </div>
+          <div class="col-md-6 col-lg-6 col-sm-12"></div>
+        </div>
+        <div class="btn-div">
+          <button class="btn mt-3 px-4 pt-2 pb-2">Book Us Now</button>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.hero {
+    height: 65vh;
+    background: url("../../assets/captain3.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    position: relative;
+    bottom: 34px;
+ 
+}
+
+.hero-text {
+    padding-top: 100px;
+    font-family: 'Roboto';
+    font-weight: 100;
+    font-style: normal;
+    font-size: 40px;
+    line-height: 50px;
+    text-transform: uppercase
+}
+h3{
+    text-transform: uppercase;
+}
+</style>>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-
+<hero></hero>
   </div>
 </template>
 
 <script>
-
+import hero from '@/components/Home/hero'
 
 
 export default {
   name: 'Home',
   components: {
- 
+hero
   }
 }
 </script>
