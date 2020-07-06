ac71ad30e573bf9a094d6a71ddaa0bb24af27b57
Prince <chimeremezechukwudire@gmail.com>
2020-07-06 15:47:46 +0100

    captain marvel component added

---
+++ src/components/Home/marvel.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="contain">
+    <div class="row">
+      <div class="col-md-7 col-lg-7">
+  <img src='../../assets/008cmv_ons_cut_mob_03.jpg'>
+      </div>
+
+     
+      <div class="col-md-4 col-lg-4 marvel">
+     <h2> captain marvel</h2>
+     <p>Near death, Carol Danvers was transformed into a powerful warrior for the Kree. Now, returning to Earth years later, she must remember her past in order to to prevent a full invasion by shapeshifting aliens, the Skrulls.</p>
+      </div>
+        <div class="col-md-1 col-lg-1">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.row{
+    margin-top: 10px;
+
+}
+img{
+    zoom:130%;
+      position:relative;
+      right: 250px;
+      bottom: 40px;
+
+}
+.marvel{
+margin-top: 180px
+}
+.marvel h2{
+    text-transform: uppercase;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,20 @@
   <div class="home">
 <hero></hero>
 <herotab></herotab>
+<marvel></marvel>
   </div>
 </template>
 
 <script>
 import hero from '@/components/Home/hero'
 import herotab from '@/components/Home/herotab'
+import marvel from '@/components/Home/marvel'
 
 
 export default {
   name: 'Home',
   components: {
-hero,herotab
+hero,herotab, marvel
   }
 }
 </script>
