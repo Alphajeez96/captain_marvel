d7ccaca45bd81cae0b0d8a1edc607297be97ea3c
Prince <chimeremezechukwudire@gmail.com>
2020-07-07 09:18:44 +0100

    latest news added

--- src/components/Home/marvel.vue
+++ src/components/Home/marvel.vue
@@ -2,36 +2,55 @@
   <div class="contain">
     <div class="row">
       <div class="col-md-7 col-lg-7">
-  <img src='../../assets/008cmv_ons_cut_mob_03.jpg'>
+        <img src="../../assets/008cmv_ons_cut_mob_03.jpg" />
       </div>
 
-     
       <div class="col-md-4 col-lg-4 marvel">
-     <h2> captain marvel</h2>
-     <p>Near death, Carol Danvers was transformed into a powerful warrior for the Kree. Now, returning to Earth years later, she must remember her past in order to to prevent a full invasion by shapeshifting aliens, the Skrulls.</p>
+        <h2 class='pb-3'>captain marvel</h2>
+        
+        <p>
+          Near death, Carol Danvers was transformed into a powerful warrior for
+          the Kree. Now, returning to Earth years later, she must remember her
+          past in order to to prevent a full invasion by shapeshifting aliens,
+          the Skrulls.
+        </p>
+
+        <div class="marvel__btn">
+          <button>Watch Now</button>
+        </div>
       </div>
-        <div class="col-md-1 col-lg-1">
-      </div>
+      <div class="col-md-1 col-lg-1"></div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.row{
-    margin-top: 10px;
-
+.row {
+  margin-top: 10px;
 }
-img{
-    zoom:130%;
-      position:relative;
-      right: 250px;
-      bottom: 40px;
-
+img {
+  zoom: 130%;
+  position: relative;
+  right: 250px;
+  bottom: 40px;
 }
-.marvel{
-margin-top: 180px
+.marvel {
+  margin-top: 180px;
 }
-.marvel h2{
-    text-transform: uppercase;
+.marvel h2 {
+  text-transform: uppercase;
+}
+.marvel__btn button {
+  border-radius: 106px 0px 106px 0px;
+  -moz-border-radius: 106px 0px 106px 0px;
+  -webkit-border-radius: 106px 0px 106px 0px;
+  border: 0px solid #000000;
+  color: #fff;
+  background-color: #e62429;
+  -webkit-transition: none;
+  transition: none;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 0 35px;
 }
 </style>
