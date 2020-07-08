5e8ed784b21c11b814cb405a36dcb605f3fa496e
Prince <chimeremezechukwudire@gmail.com>
2020-07-08 16:52:26 +0100

    footer added

--- src/components/Home/hq.vue
+++ src/components/Home/hq.vue
@@ -1,31 +1,54 @@
 <template>
-    <div class='outer'>
-    <div class='contain'>
-    <div class='con'>
-    <div class='row'>
-    <div class='col-md-6 col-lg-6'>
-    <img src='../../assets/marvelhq-promos-modules-featured-half-dsk-01.jpg'>
+  <div class="outer">
+    <div class="contain">
+      <div class="con">
+        <div class="row">
+          <div class="col-md-6 col-lg-6">
+            <img
+              src="../../assets/marvelhq-promos-modules-featured-half-dsk-01.jpg"
+            />
+          </div>
+          <div class="col-md-6 col-lg-6">
+           <div class='marvel'>
+           <h5>Marvel hq</h5>
+           <h2>Play games and read comics on MarvelHQ.com</h2>
+           <button>Check it out</button>
+           </div>
+          </div>
+        </div>
+      </div>
     </div>
-       <div class='col-md-6 col-lg-6'>
-    <img src='../../assets/marvelhq-promos-modules-featured-half-dsk-01.jpg'>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    
+  </div>
 </template>
 
 <style scoped>
-.outer{
-     width: 100%;
-    height:300px
+.outer {
+  width: 100%;
+  height: 300px;
 }
-.contain{
-    background:#151515;
-    padding:50px
+.contain {
+  background: #151515;
+  padding: 50px;
 }
-.con{
-    background: black;
+.con {
+  background: black;
 }
-</style>>
+.marvel{
+    padding: 80px
+}
+.marvel h5{
+    color: red;
+    text-transform: uppercase;
+    line-height: 40px;
+
+}
+.marvel h2{
+    color: white;
+  font-weight:bold;
+ line-height: 40px;
+
+  
+
+}
+</style>
+
---
+++ src/components/Partials/footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer>
+    <div class='row'>
+    <div class='col-md-2 col-lg-2'>
+    </div>
+    </footer>
+</template>>
