8e04fc6932f473635c16ecd6becf18a1bad7e4fb
Prince <chimeremezechukwudire@gmail.com>
2020-07-08 09:28:42 +0100

    pagination added

--- src/components/Home/latestnews.vue
+++ src/components/Home/latestnews.vue
@@ -1,14 +1,11 @@
 <template>
-
-  <div class="latest">
-   
+  <div class="latest" role="main" id="pagination-app">
     <div class="contain">
-   
       <h2>Latest news</h2>
       <div class="row">
         <div
           class="image__div col-md-3 col-lg-3"
-          v-for="event in events"
+          v-for="event in displayedPosts"
           :key="event.id"
         >
           <img
@@ -20,28 +17,44 @@
           <p>
             <v-clamp autoresize :max-lines="3">{{ event.description }}</v-clamp>
           </p>
-          <a :href="`${event.resourceURI}`" class="btn btn-primary">
+          <!-- <a :href="`${event.urls}`" class="btn btn-primary">
             Read More
-          </a>
+          </a> -->
         </div>
       </div>
+    </div>
 
-        <paginate
-    :page-count="20"
-    :page-range="3"
-    :margin-pages="2"
-    :click-handler="clickCallback"
-    :prev-text="'Prev'"
-    :next-text="'Next'"
-    :container-class="'pagination'"
-    :page-class="'page-item'">
-  </paginate>
+    <!--     Loop through the pages array to display each page number       -->
+    <div class="clearfix btn-group col-md-2 mb-3 offset-md-5">
+      <button
+        type="button"
+        class="btn btn-sm btn-outline-secondary"
+        v-if="page != 1"
+        @click="page--"
+      >
+        <<
+      </button>
+      <button
+        type="button"
+        class="btn btn-sm btn-outline-secondary"
+        v-for="pageNumber in pages.slice(page - 1, page + 5)"
+        @click="page = pageNumber"
+      >
+        {{ pageNumber }}
+      </button>
+      <button
+        type="button"
+        @click="page++"
+        v-if="page < pages.length"
+        class="btn btn-sm btn-outline-secondary"
+      >
+        >>
+      </button>
     </div>
   </div>
 </template>
 
 <script>
-
 const axios = require('axios')
 import VClamp from 'vue-clamp'
 
@@ -54,10 +67,19 @@ export default {
     return {
       apikey: '92edde42a0b45fe3b6b228a3edf4855b',
       events: [],
+      url: [],
+      page: 1,
+      perPage: 4,
+      pages: [],
     }
   },
   mounted() {
     this.getEvents()
+  },
+    watch: {
+    events () {
+      this.setPages();
+    }
   },
   methods: {
     async getEvents() {
@@ -67,14 +89,30 @@ export default {
         )
         this.events = response.data.data.results
         // console.log( response.data.data.results)
-
+        this.url = response.data
         console.log(response.data.data.results)
       } catch (error) {
         console.log(error.response)
       }
     },
-   	clickCallback: function(page) {
-      console.log(page)
+
+      setPages () {
+      let numberOfPages = Math.ceil(this.events.length / this.perPage);
+      for (let index = 1; index <= numberOfPages; index++) {
+        this.pages.push(index);
+      }
+    },  
+       paginate (events) {
+      let page = this.page;
+      let perPage = this.perPage;
+      let from = (page * perPage) - perPage;
+      let to = (page * perPage);
+      return  events.slice(from, to);
+    },
+  },
+  computed: {
+    displayedPosts () {
+      return this.paginate(this.events);
     }
   },
 }
@@ -83,7 +121,7 @@ export default {
 .latest {
   background: #151515;
   position: relative;
-  bottom:170px;
+  bottom: 170px;
 }
 
 .contain > h2 {
@@ -102,22 +140,31 @@ export default {
 }
 .image__div h5 {
   color: #999;
-   padding-top:10px
+  padding-top: 10px;
 }
 .image__div p {
   color: #fff;
   font-size: 15px;
   font-weight: bold;
   cursor: pointer;
- 
 }
 .image__div p :hover {
   color: red;
 }
 .pagination {
-  color:white
+  color: white;
 }
 .page-item {
-    color:white
+  color: white;
+}
+
+.clearfix button{
+ color:white;
+font-weight:bold
+}
+
+
+.clearfix button .btn :active{
+  color:red
 }
 </style>
